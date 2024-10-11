da73ed953bc31150c60b2a7b5d2ab2076277f924
kristo <kristoisthebest@gmail.com>
2024-10-11 23:18:18 +0900

    FE footer

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -10,7 +10,7 @@
     <title>Mitsukri Pedia</title>
 </head>
 
-<body class="m-0 font-helvetica">
+<body class="font-helvetica">
 
     <x-navbar></x-navbar>
 
@@ -279,7 +279,7 @@
 
 
     {{-- footer --}}
-    <div class="bg-biru-muda-3-0 w-full relative h-max pt-24">
+    <div class="bg-biru-muda-3-0 w-full relative pt-24 pb-12">
         <div
             class="flex items-center bg-biru-utama-0 absolute -top-28 w-max rounded-3xl justify-evenly left-1/2 transform -translate-x-1/2 shadow-md drop-shadow-md">
             <img src="{{ asset('img/brighton.png') }}" alt="" class="py-8 px-7 w-1/5 h-1/5">
@@ -288,8 +288,8 @@
                 <div class="text-sm mt-2">Support Us!</div>
             </div>
         </div>
-        <footer class="w-full h-full">
-            <ul class="mx-24 my-5 w-max">
+        <footer class="w-full grid">
+            <ul class="mx-24 w-max py-8">
                 <li class="mt-1">
                     <div class="font-semibold text-5xl text-biru-utama-0">MitsukriPedia</div>
                 </li>
@@ -319,6 +319,30 @@
                     <div class="font-medium text-sm mx-1">Linkedin</div>
                 </li>
             </ul>
+
+            <div class="grid justify-items-center">
+                <hr class="h-px mt-1 w-11/12 bg-gray-400 border-0 dark:bg-gray-700">
+            </div>
+
+            <div class="font-medium text-sm mx-1 mt-1 ml-16 text-gray-400">MitsukriPedia Ⓒ2024, All Rights Reserved
+            </div>
+
+            <div class="w-3/12 flex justify-around place-self-end mx-12">
+                <div class="bg-white p-2 rounded-2xl w-max flex items-center cursor-pointer">
+                    <img src="{{ asset('img/google.png') }}" alt="" class="w-7 h-7">
+                    <p class="mx-2">Pay</p>
+                </div>
+                <div class="bg-white p-2 rounded-2xl w-max flex items-center cursor-pointer">
+                    <img src="{{ asset('img/dana.png') }}" alt="" class="w-7 h-7">
+                    <p class="text-biru-utama-0 font-semibold text-sm mx-1">Dana</p>
+                </div>
+                <div class="bg-white p-2 rounded-2xl w-max flex items-center cursor-pointer">
+                    <img src="{{ asset('img/telkomsel.png') }}" alt="" class="w-5 h-5">
+                    <p class="text-red-500 font-semibold text-xs mx-1">Telkomsel</p>
+                </div>
+            </div>
+
+
 
         </footer>
     </div>
